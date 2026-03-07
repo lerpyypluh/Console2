2cbc0fcf144992c544ac294a6a1c9a1a2cade7d5
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-03-07 11:17:32 -0600

    update getvrrigfromplayer

--- Console.cs
+++ Console.cs
@@ -737,7 +737,7 @@ namespace Console
             world - GorillaTagger.Instance.bodyCollider.transform.position + GorillaTagger.Instance.transform.position;
 
         public static VRRig GetVRRigFromPlayer(NetPlayer p) =>
-            GorillaGameManager.instance.FindPlayerVRRig(p);
+            GorillaGameManager.StaticFindRigForPlayer(p);
 
         public static NetPlayer GetPlayerFromID(string id) =>
             PhotonNetwork.PlayerList.FirstOrDefault(player => player.UserId == id);
